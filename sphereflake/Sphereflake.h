a80dc0e91508204621bb8725fd25eb86327912be
Alexander Dzhoganov <alexander.dzhoganov@gmail.com>
2014-08-20 12:04:37 +0300

    refactor

--- sphereflake/Sphereflake.h
+++ sphereflake/Sphereflake.h
@@ -1,7 +1,7 @@
 #ifndef __RAYTRACE_SPHEREFLAKE_H
 #define __RAYTRACE_SPHEREFLAKE_H
 
-#define MAX_DEPTH 2
+#define SPHEREFLAKE_MAX_DEPTH 2
 
 namespace SphereflakeRaytracer
 {
@@ -15,7 +15,7 @@ namespace SphereflakeRaytracer
 	class Sphereflake
 	{
 
-	public:
+		public:
 		int maxDepthReached = 0;
 		long long raysPerSecond = 0;
 
@@ -63,7 +63,7 @@ namespace SphereflakeRaytracer
 			m_BottomLeft.Set(camera->GetBottomLeft());
 		}
 
-	private:
+		private:
 		void DoImagePart()
 		{
 			std::mt19937 mt;
@@ -73,6 +73,13 @@ namespace SphereflakeRaytracer
 			std::uniform_int_distribution<size_t> heightRand(0, m_Height - 2);
 
 			std::this_thread::sleep_for(std::chrono::milliseconds(10));
+
+			auto width = _mm_set1_ps((float)m_Width);
+			auto height = _mm_set1_ps((float)m_Height);
+
+			SSE::Vec3Packet position;
+			SSE::Vec3Packet normal;
+			mat4 transform = mat4(1.0f);
 
 			for (;;)
 			{
@@ -85,9 +92,6 @@ namespace SphereflakeRaytracer
 				auto x = _mm_set_ps((float)xa[3], (float)xa[2], (float)xa[1], (float)xa[0]);
 				auto y = _mm_set_ps((float)ya[3], (float)ya[2], (float)ya[1], (float)ya[0]);
 
-				auto width = _mm_set1_ps((float)m_Width);
-				auto height = _mm_set1_ps((float)m_Height);
-
 				auto uvx = _mm_div_ps(x, width);
 				auto uvy = _mm_div_ps(y, height);
 
@@ -98,24 +102,18 @@ namespace SphereflakeRaytracer
 				auto rayDirection = targetDirection - m_RayOrigin;
 				Normalize(rayDirection);
 
-				Vec3Packet position;
 				position.Set(vec3(0.0f));
-
-				Vec3Packet normal;
 				normal.Set(vec3(0.0f));
 
-				mat4 transform = mat4(1.0f);
 				__m128 minT = _mm_set1_ps(std::numeric_limits<float>::max());
 
-				auto result = RenderSphereflake(m_RayOrigin, rayDirection, transform, 3.f, 0, minT, position, normal);
+				RenderSphereflake(m_RayOrigin, rayDirection, transform, 3.f, 0, minT, position, normal);
 
 				for (auto q = 0u; q < 4; q++)
 				{
 					auto idx = xa[q] + ya[q] * m_Width;
-					auto rposition = vec4(position.Extract(q), 1.0f);
-					auto rnormal = vec4(normal.Extract(q), 1.0f);
-					m_GBuffer.positions[idx] = rposition;
-					m_GBuffer.normals[idx] = rnormal;
+					m_GBuffer.positions[idx] = vec4(position.Extract(q), 1.0f);
+					m_GBuffer.normals[idx] = vec4(normal.Extract(q), 1.0f);
 					raysPerSecond++;
 				}
 
@@ -130,6 +128,13 @@ namespace SphereflakeRaytracer
 		{
 			auto sint = sinf(longitude);
 			return vec3(cosf(latitude) * sint, sinf(latitude) * sint, cosf(longitude));
+		}
+
+		inline mat4 CreateRotationMatrix(const vec3& rot)
+		{
+			return glm::rotate(radians(rot.x), vec3(1, 0, 0)) *
+				glm::rotate(radians(rot.y), vec3(0, 1, 0)) *
+				glm::rotate(radians(rot.z), vec3(0, 0, 1));
 		}
 
 		void PrecomputeChildTransforms()
@@ -168,54 +173,49 @@ namespace SphereflakeRaytracer
 			}
 		}
 
-		mat4 CreateRotationMatrix(const vec3& rot)
-		{
-			return glm::rotate(radians(rot.x), vec3(1, 0, 0)) *
-				glm::rotate(radians(rot.y), vec3(0, 1, 0)) *
-				glm::rotate(radians(rot.z), vec3(0, 0, 1));
-		}
-
 		inline __m128 RenderSphereflake
-			(
-			const Vec3Packet& rayOrigin,
-			const Vec3Packet& rayDirection,
+		(
+			const SSE::Vec3Packet& rayOrigin,
+			const SSE::Vec3Packet& rayDirection,
 			const mat4& parentTransform,
 			float parentRadius,
 			int depth,
 			__m128& minT,
-			Vec3Packet& position,
-			Vec3Packet& normal
-			)
+			SSE::Vec3Packet& position,
+			SSE::Vec3Packet& normal
+		)
 		{
-			if (depth >= MAX_DEPTH)
+			if (depth >= SPHEREFLAKE_MAX_DEPTH)
 			{
-				return SSEConstants::zero;
+				return SSE::Constants::zero;
 			}
 
 			float radiusScalar = parentRadius / 3.0f;
 			__m128 radius = _mm_set1_ps(radiusScalar);
 			__m128 radiusSq = _mm_mul_ps(radius, radius);
 
-			__m128 doubleRadiusSq = _mm_mul_ps(radius, SSEConstants::two);
+			__m128 doubleRadiusSq = _mm_mul_ps(radius, SSE::Constants::two);
 			doubleRadiusSq = _mm_mul_ps(doubleRadiusSq, doubleRadiusSq);
 
 			const vec3& sphereOrigin = vec3(parentTransform[3]);
 
-			Vec3Packet sphereOriginPacket;
+			SSE::Vec3Packet sphereOriginPacket;
 			sphereOriginPacket.Set(sphereOrigin);
 
 			__m128 t;
 			auto result = RaySphereIntersection(rayOrigin, rayDirection, sphereOriginPacket, doubleRadiusSq, t);
 			if (_mm_movemask_ps(result) == 0)
 			{
+				// all rays miss bounding sphere
 				return result;
 			}
 
-			auto depthResult = _mm_cmplt_ps(_mm_sqrt_ps(_mm_div_ps(t, radius)), SSEConstants::hundred);
-			auto tLessThanZeroResult = _mm_cmplt_ps(t, SSEConstants::zero);
+			auto depthResult = _mm_cmplt_ps(_mm_sqrt_ps(_mm_div_ps(t, radius)), SSE::Constants::hundred);
+			auto tLessThanZeroResult = _mm_cmplt_ps(t, SSE::Constants::zero);
 
 			if (_mm_movemask_ps(_mm_or_ps(depthResult, tLessThanZeroResult)) == 0)
 			{
+				// sphere is behind all rays or depth is too large
 				return result;
 			}
 
@@ -238,36 +238,26 @@ namespace SphereflakeRaytracer
 
 			result = RaySphereIntersection(rayOrigin, rayDirection, sphereOriginPacket, radiusSq, t);
 
+			// depth comparison
 			auto minTResult = _mm_cmplt_ps(t, minT);
 			result = _mm_and_ps(result, minTResult);
 
 			if (_mm_movemask_ps(result) == 0)
 			{
+				// all rays don't pass depth test
 				return result;
 			}
 
 			minT = _mm_or_ps(_mm_andnot_ps(result, minT), _mm_and_ps(result, t));
 
-			auto rPosition = rayOrigin + (rayDirection * t);
+			// calculate resulting position and normal
+			auto selfPosition = rayOrigin + (rayDirection * t);
+			auto selfNormal = selfPosition - sphereOriginPacket;
+			SSE::Normalize(selfNormal);
 
-			auto pPositionX = _mm_andnot_ps(result, position.x);
-			auto pPositionY = _mm_andnot_ps(result, position.y);
-			auto pPositionZ = _mm_andnot_ps(result, position.z);
-
-			position.x = _mm_or_ps(pPositionX, _mm_and_ps(result, rPosition.x));
-			position.y = _mm_or_ps(pPositionY, _mm_and_ps(result, rPosition.y));
-			position.z = _mm_or_ps(pPositionZ, _mm_and_ps(result, rPosition.z));
-
-			auto rNormal = rPosition - sphereOriginPacket;
-			Normalize(rNormal);
-
-			auto pNormalX = _mm_andnot_ps(result, normal.x);
-			auto pNormalY = _mm_andnot_ps(result, normal.y);
-			auto pNormalZ = _mm_andnot_ps(result, normal.z);
-
-			normal.x = _mm_or_ps(pNormalX, _mm_and_ps(result, rNormal.x));
-			normal.y = _mm_or_ps(pNormalY, _mm_and_ps(result, rNormal.y));
-			normal.z = _mm_or_ps(pNormalZ, _mm_and_ps(result, rNormal.z));
+			// mask results
+			position = SSE::BitwiseOr(SSE::BitwiseAndNot(result, position), SSE::BitwiseAnd(result, selfPosition));
+			normal = SSE::BitwiseOr(SSE::BitwiseAndNot(result, normal), SSE::BitwiseAnd(result, selfNormal));
 			return result;
 		}
 
@@ -280,10 +270,10 @@ namespace SphereflakeRaytracer
 
 		bool m_Deinitialize = false;
 
-		Vec3Packet m_RayOrigin;
-		Vec3Packet m_TopLeft;
-		Vec3Packet m_TopRight;
-		Vec3Packet m_BottomLeft;
+		SSE::Vec3Packet m_RayOrigin;
+		SSE::Vec3Packet m_TopLeft;
+		SSE::Vec3Packet m_TopRight;
+		SSE::Vec3Packet m_BottomLeft;
 
 	};
 
